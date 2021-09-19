c946b449b7bacb5499b9eb81128307008c4f0d60
Ravi Wu <raviwu@gmail.com>
2021-09-19 16:36:44 +0800

    Bind event to button

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+        };
+    },
+    methods: {
+        add() {
+            this.counter += 1;
+        },
+        remove() {
+            this.counter -= 1;
+        }
+    }
+});
+
+app.mount('#events');
