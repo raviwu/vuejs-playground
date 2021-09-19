954a53cc9e67ea43576f0f9332011138c6c5f48a
Ravi Wu <raviwu@gmail.com>
2021-09-19 16:49:55 +0800

    Binding and passing argument from template

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,15 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
+            num: 1,
             counter: 0,
+            name: ''
         };
     },
     methods: {
+        setNum(event) {
+            this.num = parseInt(event.target.value);
+        },
         add() {
-            this.counter += 1;
+            this.counter += this.num;
         },
         remove() {
-            this.counter -= 1;
+            this.counter -= this.num;
+        },
+        setName(event, lastName) {
+            this.name = event.target.value + ' ' + lastName;
         }
     }
 });
